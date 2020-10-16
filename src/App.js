3bb7159a7899eff5c102e68d1d39e35fe9c502a1
Varun Naik <varun@naikvarun.org>
2020-10-16 07:59:21 -0500

    created dev docker file and docker compose

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Docker and K8s
         </a>
       </header>
     </div>
